f618bfe1d4ca20b050c3f233b281a5ce7d3a3922
Travis Kent Beste <travis@tencorners.com>
2019-11-06 15:09:16 -0600

    adding POJOs. Closes #2.

---
+++ src/main/java/com/tencorners/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.tencorners.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/tencorners/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.tencorners.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
