adaea058360b81bd6e3af16c7615d6ef5ea0a76f
Travis Kent Beste <travis@tencorners.com>
2019-11-09 13:59:46 -0600

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/tencorners/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package com.tencorners.sfgpetclinic.services;
+
+import com.tencorners.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
