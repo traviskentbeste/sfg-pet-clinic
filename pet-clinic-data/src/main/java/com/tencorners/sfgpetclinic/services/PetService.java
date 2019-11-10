61b7bf555c22ef73029aa51c6b48865f64be056a
Travis Kent Beste <travis@tencorners.com>
2019-11-10 13:37:26 -0600

    refactored to common crud service. closes #11

--- pet-clinic-data/src/main/java/com/tencorners/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/tencorners/sfgpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.tencorners.sfgpetclinic.services;
 
 import com.tencorners.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>
+{
 }
