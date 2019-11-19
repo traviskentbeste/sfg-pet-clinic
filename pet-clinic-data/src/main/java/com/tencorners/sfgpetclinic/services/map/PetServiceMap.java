4c614710d30834ee944c645629b0c102ed1b4dc9
Travis Kent Beste <travis@tencorners.com>
2019-11-19 14:07:47 -0600

    added Pets and Contact info to owneres on startup. closes #33

--- pet-clinic-data/src/main/java/com/tencorners/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/tencorners/sfgpetclinic/services/map/PetServiceMap.java
@@ -1,13 +1,13 @@
 package com.tencorners.sfgpetclinic.services.map;
 
 import com.tencorners.sfgpetclinic.model.Pet;
-import com.tencorners.sfgpetclinic.services.CrudService;
+import com.tencorners.sfgpetclinic.services.PetService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long>
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService
 {
     @Override
     public Set<Pet> findAll() {
--- pet-clinic-web/src/main/java/com/tencorners/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/tencorners/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.tencorners.sfgpetclinic.bootstrap;
 
 import com.tencorners.sfgpetclinic.model.Owner;
+import com.tencorners.sfgpetclinic.model.Pet;
 import com.tencorners.sfgpetclinic.model.PetType;
 import com.tencorners.sfgpetclinic.model.Vet;
 import com.tencorners.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.tencorners.sfgpetclinic.services.PetTypeService;
 import com.tencorners.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -38,12 +41,30 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("123-456-7890");
 
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedCatPetType);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("123 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("123-456-7890");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setName("Just Cat");
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(savedCatPetType);
+        owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
 
