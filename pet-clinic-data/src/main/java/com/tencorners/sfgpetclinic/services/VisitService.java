c315bd905e6a3cc8756f2dfafcbdf5cbe951408e
Travis Kent Beste <travis@tencorners.com>
2019-11-26 00:47:19 -0600

    Implement map impl for visit service. closes #46

---
+++ pet-clinic-data/src/main/java/com/tencorners/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.tencorners.sfgpetclinic.services;
+
+import com.tencorners.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/tencorners/sfgpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,39 @@
+package com.tencorners.sfgpetclinic.services.map;
+
+import com.tencorners.sfgpetclinic.model.Visit;
+import com.tencorners.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if (object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
